9719f3a7a650b2529af8ecf3f3b3bec01789934b
Lukas Feuer <feuerlukas@web.de>
2024-10-31 19:27:58 +0100

    Zwischenstand - Renten

--- R/02 Renten.R
+++ R/02 Renten.R
@@ -9,25 +9,48 @@
 #
 
 library(tidyverse)
-library(lubridate)
+library(GGally)
+
+thematic::thematic_off()
+
 
 d_raw <- readxl::read_xlsx("data/Renten.xlsx")
 
-d_raw |>
+d <- d_raw |>
   mutate(
     Nummer = NULL
-    , Rente_Beginn_Datum = as_date(Rente_Beginn_Datum, origin = "1899-12-30 UTC")
-    , Rente_Ende_Datum = as_date(Rente_Ende_Datum, origin = "1899-12-30 UTC")
-    , Geburtsdatum = as_date(Geburtsdatum, origin = "1899-12-30 UTC")
-    , Geburtsjahr = year(Geburtsdatum)
+    , Segment = factor(Segment)
+    , Rente_Beginn_Datum = lubridate::as_date(Rente_Beginn_Datum, origin = "1899-12-30 UTC")
+    , Rente_Ende_Datum = lubridate::as_date(Rente_Ende_Datum, origin = "1899-12-30 UTC")
+    , Geburtsdatum = lubridate::as_date(Geburtsdatum, origin = "1899-12-30 UTC")
+    , Geburtsjahr = lubridate::year(Geburtsdatum)
     , Geschlecht = factor(Geschlecht
                           , levels = c(2, 1, NA)
                           , labels = c("Weiblich", "Männlich"))
-    , Rentendauer = interval(Rente_Beginn_Datum, Rente_Ende_Datum) %/% years(1)
-    , Rentenalter = interval(Geburtsdatum, Rente_Beginn_Datum) %/% years(1)
-    , Ort = str_extract(Adresse, "\\w+$")
-    , PLZ = str_extract(Adresse, "[:space:]\\d{5}[:space:]")
+    , Rentendauer = lubridate::interval(Rente_Beginn_Datum, Rente_Ende_Datum) %/% lubridate::years(1)
+    , Rentenalter = lubridate::interval(Geburtsdatum, Rente_Beginn_Datum) %/% lubridate::years(1)
+    , Ort = factor(str_extract(Adresse, "\\w+$"))
+    , PLZ = factor(str_extract(Adresse, "[:space:]\\d{5}[:space:]"))
   )
+
+
+GGally::ggpairs(d[, c("Renten_Auszahlung", "Geschlecht", "Segment", "Geburtsjahr")])
+
+x <- d |>
+  filter(Renten_Auszahlung < 30000) |>
+  select("Renten_Auszahlung", "Geschlecht", "Segment", "Geburtsjahr", "Rentenalter") #
+
+
+GGally::ggpairs(x)
+
+GGally::ggpairs(x, mapping = aes(colour = Segment, alpha = 0.5))
+
+x |> filter(!is.na(Geschlecht), Segment == "Unfall") |>
+GGally::ggpairs(mapping = aes(colour = Geschlecht, alpha = 0.5))
+
+x |> filter(!is.na(Geschlecht)) |>
+GGally::ggpairs(mapping = aes(colour = Geschlecht, alpha = 0.5))
+
 
 x |>
   ggplot(aes(Segment, Rentenalter, group = Segment)) +
@@ -39,6 +62,21 @@ x |>
   geom_histogram() +
   facet_wrap(vars(Segment))
 
+
+d |>
+  mutate(kategorie = case_when(
+    str_detect(Segment, "haft") ~ "Haftpflichten"
+    , TRUE ~ "Unfall")) |>
+  reframe(Rentenauszahlung = sum(Renten_Auszahlung), .by = kategorie)
+
+# ggf. sieht das noch eindeutiger aus, wenn "Rentenalter" nicht nur grob auf Jahr gerechnet
+d |> filter(!is.na(Rente_Ende_Datum)) |>
+  ggplot(aes(Rentenalter, Rentendauer))
+  + geom_point()
+
+d |> count(Rentendauer, Segment, sort = T) |> print(n = 50)
+
+
 ## TODO
 ## - Adressen aufteilen - Darstellung geographische Verteilung
 ## - Kontrolle Datumsumwandlung
@@ -48,3 +86,9 @@ x |>
 ## - Modellierung (AV Renten_Auszahlung?)
 ## - Histogramme, z.B. AV nach Geschlecht, Segment
 ## - Rentenauszahlung monatlich/Jährlich? in dem Fall zu multiplizieren mit Rentenlänge
+## - Räumliche Darstellung Rentenauszahlung/Häufigkeit nach PLZ/Ort auf Karte
+## - Rentendauer in Monate/Wochen/Tage umrechenen -> Muster?
+## - Inhaltlich: wann/wem wird die Rente ausgezahlt?
+## - Warum habe so viele alte Personen die Unfallrente aber in anderen Bereichen sind es viele junge Leute?
+## - Was ist mit den Einträgen ohne Geschlechtsangabe? tendenziell viele hohe Renten --> ignorieren -> sind nur 5
+##
