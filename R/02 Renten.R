4049e0019450a87fa565651ba894d0f723688497
Lukas Feuer <feuerlukas@web.de>
2024-10-31 22:44:08 +0100

    Analyse Rente - Plots

--- R/02 Renten.R
+++ R/02 Renten.R
@@ -69,6 +69,10 @@ d |>
     , TRUE ~ "Unfall")) |>
   reframe(Rentenauszahlung = sum(Renten_Auszahlung), .by = kategorie)
 
+d |>
+  reframe(Rentenauszahlung = sum(Renten_Auszahlung), .by = Segment)
+
+
 # ggf. sieht das noch eindeutiger aus, wenn "Rentenalter" nicht nur grob auf Jahr gerechnet
 d |> filter(!is.na(Rente_Ende_Datum)) |>
   ggplot(aes(Rentenalter, Rentendauer))
@@ -76,6 +80,62 @@ d |> filter(!is.na(Rente_Ende_Datum)) |>
 
 d |> count(Rentendauer, Segment, sort = T) |> print(n = 50)
 
+
+# warum sind die Startdaten der Renten so seltsam verteilt?
+d |>
+  select(Rente_Beginn_Datum, Segment) |>
+  transmute(
+    ym = year(Rente_Beginn_Datum)
+    , Segment
+  ) |>
+  count(ym, Segment) |>
+  tsibble(index = ym, key = Segment) |>
+  autoplot() |>
+  plotly::ggplotly()
+
+# ggf. erklärt sich die Verteilung oben aus den Geburtsdaten
+# Bsp. Unfall: hier scheint es sich um eine sehr weite Koohrte zu handeln
+d |>
+  select(Geburtsdatum, Segment) |>
+  transmute(
+    ym = year(Geburtsdatum)
+    , Segment
+  ) |>
+  count(ym, Segment) |>
+  tsibble(index = ym, key = Segment) |>
+  autoplot() |>
+  plotly::ggplotly()
+
+p1 <- d |>
+  ggplot(aes(Rente_Beginn_Datum, Geburtsdatum, colour = Segment)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, colour = "lightgray")
+plotly::ggplotly(p1)
+# Berücksichtige die unterschiedlichen Verteilungsmuster pro Segment
+# Heilwesen: hier gibt es auch Fälle, bei denen der Rentenbeginn mehrere Jahre vor der Geburt liegt
+# so einen FAll gibt es auch bei Unfall
+
+p2 <- d |>
+  ggplot(aes(Rente_Beginn_Datum, Rentenalter, colour = Segment)) +
+  geom_point() +
+  geom_abline(slope = 0, intercept = 0, colour = "lightgray")
+plotly::ggplotly(p2)
+# Warum beginnen so viele Unfall-Renten mit dem gesetzlichen
+# Rentenalter?
+# -----> gesetzliche vs. Private Unfallversicherung
+# bzgl. Heilwesen: HDI hat laut Website deutlich länger Erfahrung in diesem Bereich
+# , das kann also nicht alles sein --> nur eine Kohorte/ein Produkt?
+
+# keine ersichtliche Saisonalität
+d |>
+  select(Rente_Beginn_Datum) |>
+  transmute(
+    ym = yearquarter(Rente_Beginn_Datum)
+  ) |>
+  count(ym) |>
+  tsibble(index = ym) |>
+  fill_gaps(n = 0) |>
+  gg_subseries(n)
 
 ## TODO
 ## - Adressen aufteilen - Darstellung geographische Verteilung
@@ -91,4 +151,5 @@ d |> count(Rentendauer, Segment, sort = T) |> print(n = 50)
 ## - Inhaltlich: wann/wem wird die Rente ausgezahlt?
 ## - Warum habe so viele alte Personen die Unfallrente aber in anderen Bereichen sind es viele junge Leute?
 ## - Was ist mit den Einträgen ohne Geschlechtsangabe? tendenziell viele hohe Renten --> ignorieren -> sind nur 5
-##
+## - Rentenbeginn als Zeitreihe umrechnen
+## - ggf. auch Geburtsdaten als Zeitreiehe --> verzerrt da jüngere Geburtstage weniger vorkommen werden
